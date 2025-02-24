24e9d6a3eced1632b60f0bcda156e8ec656e9a84
ttewabe <tomtadege@gmail.com>
2025-02-23 23:11:29 -0800

    integrate React into angular app via web component

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project';
+  formTitle:string = 'Pass React Props to angular components';
 }
--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA],// To use custom elements in our application
   providers: [],
   bootstrap: [AppComponent]
 })
